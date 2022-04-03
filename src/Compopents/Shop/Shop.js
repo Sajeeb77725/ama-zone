32b5264a683d0519ea8fa2ddc4014699ad7f6a18
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2022-04-03 17:33:50 +0600

    added hooks

--- src/Compopents/Shop/Shop.js
+++ src/Compopents/Shop/Shop.js
@@ -1,67 +1,62 @@
-import { faExternalLinkSquare } from '@fortawesome/free-solid-svg-icons';
-import React, { useEffect, useState } from 'react';
-import { addToDb, getStroedCart } from '../../utilities/fakedb';
-import Cart from '../Cart/Cart';
-import Product from '../Product/Product';
-import "./Shop.css"
+import { faExternalLinkSquare } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect, useState } from "react";
+import useProducts from "../../hooks/useProducts";
+import { addToDb, getStroedCart } from "../../utilities/fakedb";
+import Cart from "../Cart/Cart";
+import Product from "../Product/Product";
+import "./Shop.css";
 
 const Shop = () => {
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
-    useEffect(() =>{
-        fetch('products.json')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, []);
+  const [products, setProducts] = useProducts();
+  const [cart, setCart] = useState([]);
 
-    useEffect(() =>{
-        const stroedCart = getStroedCart();
-        const savedCart = [];
-        for(const id in stroedCart){
-            const addedProduct = products.find(product => product.id === id)
-            if(addedProduct){
-                const queantity = stroedCart[id];
-                addedProduct.queantity = queantity;
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart)
-    },[products])
+  useEffect(() => {
+    const stroedCart = getStroedCart();
+    const savedCart = [];
+    for (const id in stroedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const queantity = stroedCart[id];
+        addedProduct.queantity = queantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setCart(savedCart);
+  }, [products]);
 
-    const handleAddToCart = (product) =>{
-        // console.log(product);
-        let newCart = [];
-        const exist = cart.find(productOne => productOne.id === product.id);
-        if(!exist){
-            product.queantity = 1;
-            newCart = [...cart, product];
-        }
-        else{
-            const rest = cart.filter(productOne => productOne.id !== product.id);
-            exist.queantity = exist.queantity + 1;
-            newCart = [...rest, exist];
-        }
-        
-        setCart(newCart);
-        addToDb(product.id);
+  const handleAddToCart = (product) => {
+    // console.log(product);
+    let newCart = [];
+    const exist = cart.find((productOne) => productOne.id === product.id);
+    if (!exist) {
+      product.queantity = 1;
+      newCart = [...cart, product];
+    } else {
+      const rest = cart.filter((productOne) => productOne.id !== product.id);
+      exist.queantity = exist.queantity + 1;
+      newCart = [...rest, exist];
     }
 
-    return (
-        <div className="shopping">
-            <div className="product-container">
-                {
-                    products.map(product => <Product 
-                    key={product.id}
-                    handleAddToCart = {handleAddToCart}
-                    product = {product}
-                    ></Product>)
-                }
-            </div>
-            <div className="order-container">
-                <Cart cart={cart}></Cart>
-            </div>
-        </div>
-    );
+    setCart(newCart);
+    addToDb(product.id);
+  };
+
+  return (
+    <div className="shopping">
+      <div className="product-container">
+        {products.map((product) => (
+          <Product
+            key={product.id}
+            handleAddToCart={handleAddToCart}
+            product={product}
+          ></Product>
+        ))}
+      </div>
+      <div className="order-container">
+        <Cart cart={cart}></Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
---
+++ src/hooks/useCart.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import { getStroedCart } from "../utilities/fakedb";
+
+const useCart = (products) => {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    const storedCart = getStroedCart();
+    const savedCart = [];
+    for (const id in storedCart) {
+      const addedProduct = products.find((product) => product.id === id);
+      if (addedProduct) {
+        const queantity = storedCart[id];
+        addedProduct.queantity = queantity;
+        savedCart.push(addedProduct);
+      }
+    }
+    setCart(savedCart);
+  }, [products]);
+
+  return [cart, setCart];
+};
+
+export default useCart;
