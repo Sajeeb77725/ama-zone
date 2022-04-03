7140746225e8d3be5de4d5bde51ce67e507f6686
Sajeeb77725 <mandal15-4045@diu.edu.bd>
2022-04-03 15:56:43 +0600

    route first one

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-
-import './App.css';
-import Header from './Compopents/Header/Header';
-import Shop from './Compopents/Shop/Shop';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Compopents/About/About";
+import Header from "./Compopents/Header/Header";
+import Inventory from "./Compopents/Inventory/Inventory";
+import Orders from "./Compopents/Orders/Orders";
+import Shop from "./Compopents/Shop/Shop";
 
 function App() {
   return (
     <div>
       <Header></Header>
-      <Shop></Shop>
+      <Routes>
+        <Route path="/" element={<Shop></Shop>}></Route>
+        <Route path="/shopping" element={<Shop></Shop>}></Route>
+        <Route path="/orders" element={<Orders></Orders>}></Route>
+        <Route path="/inventory" element={<Inventory></Inventory>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
     </div>
   );
 }
